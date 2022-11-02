19e8a3b0eb85d6f6d21826ac0965ac1ac23fc448
chenlianghong <clh021@gmail.com>
2022-11-02 22:16:06 +0800

    add options.lua

--- chadrc.lua
+++ chadrc.lua
@@ -2,12 +2,14 @@ local M = {}
 
 M.ui = {
   theme_toggle = { "onedark", "one_light" },
-  theme = "onedark",
+  theme = "one_light",
 }
 
 M.plugins = require "custom.plugins"
 
 -- check core.mappings for table structure
 M.mappings = require "custom.mappings"
+
+M.options = require "custom.options"
 
 return M
---
+++ options.lua
@@ -0,0 +1,4 @@
+return {
+  -- autosave = true, -- 没有看到效果，不知触发条件
+}
+
