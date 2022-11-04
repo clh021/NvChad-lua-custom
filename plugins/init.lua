c151a66fdee2c40a1b4f784c817b01da8bee693b
chenlianghong <clh021@gmail.com>
2022-11-04 23:19:05 +0800

    config golang gopls failed

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,2 +1,78 @@
-return {
+-- local overrides = require "custom.plugins.overrides"
+-- return {
+--   -- ["goolord/alpha-nvim"] = { disable = false } -- enables dashboard
+--
+--   -- Override plugin definition options
+--   ["neovim/nvim-lspconfig"] = {
+--     config = function()
+--       require "plugins.configs.lspconfig"
+--       require "custom.plugins.lspconfig"
+--     end,
+--   },
+--
+--   -- https://github.com/wbthomason/packer.nvim#specifying-plugins
+--   -- overrde plugin configs
+--   -- ["nvim-treesitter/nvim-treesitter"] = {
+--   --   commit = "e53950f646b0c11624280ee7c4eff97a9e0904f1",
+--   --   override_options = custom.plugins.treesitter,
+--   --   config = function()
+--   --       require("nvim-treesitter.install").prefer_git = true
+--   --   end,
+--   -- },
+--
+--   ["williamboman/mason.nvim"] = {
+--     override_options = overrides.mason,
+--   },
+--
+--   -- ["kyazdani42/nvim-tree.lua"] = {
+--   --   override_options = overrides.nvimtree,
+--   -- },
+--   --
+--   -- -- Install a plugin
+--   -- ["max397574/better-escape.nvim"] = {
+--   --   event = "InsertEnter",
+--   --   config = function()
+--   --     require("better_escape").setup()
+--   --   end,
+--   -- },
+--
+--   -- code formatting, linting etc
+--   -- ["jose-elias-alvarez/null-ls.nvim"] = {
+--   --   after = "nvim-lspconfig",
+--   --   config = function()
+--   --     require "custom.plugins.null-ls"
+--   --   end,
+--   -- },
+--
+--   -- remove plugin example
+--   -- ["hrsh7th/cmp-path"] = false,
+-- }
+
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
+local lspconfig = require('lspconfig')
+
+lspconfig['pyright'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+lspconfig['tsserver'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+lspconfig['rust_analyzer'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+    -- Server-specific settings...
+    settings = {
+      ["rust-analyzer"] = {}
+    }
+}
+lspconfig['gopls'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
 }
--- plugins/overrides.lua
+++ plugins/overrides.lua
@@ -1,71 +1,71 @@
-local M = {}
-
-M.treesitter = {
-  -- :TSUpdate all
-  -- :TSUpdate
-  auto_install = true,
-  ensure_installed = {
-    "bash",
-    "c",
-    "cmake",
-    "cpp",
-    "css",
-    "diff",
-    "dockerfile",
-    "gitignore",
-    "go",
-    "html",
-    "javascript",
-    "json",
-    "lua",
-    "make",
-    "markdown",
-    "markdown_inline",
-    "php",
-    "proto",
-    "python",
-    "scss",
-    "sql",
-    "typescript",
-    "vim",
-    "vue",
-    "yaml",
-  },
-}
-
-M.mason = {
-  -- https://github.com/williamboman/mason-lspconfig.nvim
-  ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
-
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    -- "deno",
-    "dockerfile-language-server",
-    "gopls",
-    "vue-language-server",
-
-  },
-}
-
--- git support in nvimtree
-M.nvimtree = {
-  git = {
-    enable = true,
-  },
-
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
-}
-
-return M
+-- local M = {}
+--
+-- M.treesitter = {
+--   -- :TSUpdate all
+--   -- :TSUpdate
+--   auto_install = true,
+--   ensure_installed = {
+--     "bash",
+--     "c",
+--     "cmake",
+--     "cpp",
+--     "css",
+--     "diff",
+--     "dockerfile",
+--     "gitignore",
+--     "go",
+--     "html",
+--     "javascript",
+--     "json",
+--     "lua",
+--     "make",
+--     "markdown",
+--     "markdown_inline",
+--     "php",
+--     "proto",
+--     "python",
+--     "scss",
+--     "sql",
+--     "typescript",
+--     "vim",
+--     "vue",
+--     "yaml",
+--   },
+-- }
+--
+-- M.mason = {
+--   -- https://github.com/williamboman/mason-lspconfig.nvim
+--   ensure_installed = {
+--     -- lua stuff
+--     "lua-language-server",
+--     "stylua",
+--
+--     -- web dev stuff
+--     "css-lsp",
+--     "html-lsp",
+--     "typescript-language-server",
+--     -- "deno",
+--     "dockerfile-language-server",
+--     "gopls",
+--     "vue-language-server",
+--
+--   },
+-- }
+--
+-- -- git support in nvimtree
+-- M.nvimtree = {
+--   git = {
+--     enable = true,
+--   },
+--
+--   renderer = {
+--     highlight_git = true,
+--     icons = {
+--       show = {
+--         git = true,
+--       },
+--     },
+--   },
+-- }
+--
+-- return M
