742d48cabdf01b54851731cad85cb114ec61b2e0
chenlianghong <clh021@gmail.com>
2022-11-22 12:19:27 +0800

    lspconfig: add vls options prepare

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -27,8 +27,44 @@ lspconfig.gopls.setup {
   },
 }
 
-lspconfig.gopls.setup {
+lspconfig.vls.setup {
   cmd = {"vls"},
   filetypes = {"vue"},
   root_dir = lspconfig.util.root_pattern("package.json", "vue.config.js")
+  -- init_options = {
+    -- css = {},
+    -- emmet = {},
+    -- html = {
+    --   suggest = {}
+    -- },
+    -- javascript = {
+    --   format = {}
+    -- },
+    -- stylusSupremacy = {},
+    -- typescript = {
+    --   format = {}
+    -- },
+    -- vetur = {
+    --   completion = {
+    --     autoImport = false,
+    --     tagCasing = "kebab",
+    --     useScaffoldSnippets = false
+    --   },
+    --   format = {
+    --     defaultFormatter = {
+    --       js = "none",
+    --       ts = "none"
+    --     },
+    --     defaultFormatterOptions = {},
+    --     scriptInitialIndent = false,
+    --     styleInitialIndent = false
+    --   },
+    --   useWorkspaceDependencies = false,
+    --   validation = {
+    --     script = true,
+    --     style = true,
+    --     template = true
+    --   }
+    -- }
+  -- }
 }
