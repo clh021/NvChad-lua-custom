4ad56eabd25fbc6571f169c4a00cef1111865b36
chenlianghong <clh021@gmail.com>
2022-11-23 10:09:11 +0800

    test: ok to test

--- test/test.nvim.sh
+++ test/test.nvim.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 # leehom Chen clh021@gmail.com
-# :Tutor
-# help docs
-# :terminal
-# :h autocommand
-# :h vim.api
-# :h insert
-
-# install language highlighting
-# :TSInstall css html
-
-# :Telescope keymaps
-docker images | grep none | awk '{ print $3; }' | xargs docker rmi --force
 docker build -t test.nvim .
+docker images | grep none | awk '{ print $3; }' | xargs docker rmi --force
+docker run -it --rm --hostname=NVimTest test.nvim bash
