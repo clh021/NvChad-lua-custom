5979dd7d79572f84bb64f4724ecd3c45bcaf7d9a
chenlianghong <clh021@gmail.com>
2022-11-23 11:06:10 +0800

    test: fix userPWD & pkg

--- test/run.docker.in.host.sh
+++ test/run.docker.in.host.sh
@@ -3,4 +3,4 @@
 docker build -t test.nvim .
 docker images | grep none | awk '{ print $3; }' | xargs docker rmi --force
 # docker cp -r `pwd` nvim:`pwd`
-docker run -it --rm ---name nvim --hostname=nvim test.nvim bash
+docker run -it --rm --name nvim --hostname=nvim test.nvim bash
